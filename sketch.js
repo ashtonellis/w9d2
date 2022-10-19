d7c0b7422d693ae4616a8e06f23a872c0e0169f9
Ashton Yeackley <aey21@fsu.edu>
2022-10-19 13:32:29 -0400

    if then else

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let canvasWidth = 400;
+const canvasHeight = 400;
+let x = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasWidth, canvasHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(125);
+  if (x > 100 || x < 200) {
+    rect(x,x,50,50);
+  }
+  x++;
+  // rect(x*2,x*2,25,25);
 }
